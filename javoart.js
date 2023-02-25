92158732e712a0975d3d55457fcf69b77222684d
Jose A Diaz <88800637+Souleater6766@users.noreply.github.com>
2023-02-25 13:40:33 -0600

    Add files via upload

---
+++ javoart.js
@@ -0,0 +1,26 @@
+    <div class="console">
+      <img src="https://via.placeholder.com/200x200.png?text=Console+2" alt="Console 2">
+      <h3>Console 2</h3>
+      <p>Proin eu justo auctor, posuere velit eu, vestibulum ipsum. Aliquam in risus non arcu sodales feugiat vitae id sapien.</p>
+      <button>Learn More</button>
+    </div>
+    <div class="console">
+      <img src="https://via.placeholder.com/200x200.png?text=Console+3" alt="Console 3">
+      <h3>Console 3</h3>
+      <p>Maecenas ullamcorper nunc vel libero fermentum, eu bibendum nunc auctor. Nulla facilisi. Morbi lobortis augue sit amet lacinia facilisis.</p>
+      <button>Learn More</button>
+    </div>
+    <div class="console">
+      <img src="https://via.placeholder.com/200x200.png?text=Console+4" alt="Console 4">
+      <h3>Console 4</h3>
+      <p>Ut vehicula justo in dui maximus, sed scelerisque enim vestibulum. Fusce et ornare purus.</p>
+      <button>Learn More</button>
+    </div>
+  </section>
+</main>
+<footer>
+  <p>&copy; 2023 Retro Games and Consoles. All rights reserved.</p>
+</footer>
+</body>
+</html>
+```
